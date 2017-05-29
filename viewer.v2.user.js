fbf698c1ab43dc1aa5669c4b4804bb5a4a4ebbf3
Vihan <vihanb@users.noreply.github.com>
2017-05-29 10:22:32 -0700

    Fixed HTTPS issues

--- viewer.v2.user.js
+++ viewer.v2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sandbox Viewer
 // @namespace    https://github.com/vihanb/PPCG-SandboxViewer
-// @version      2.3
+// @version      2.4
 // @description  PPCG Sandbox Viewer
 // @author       Downgoat
 // @match        *://*.stackexchange.com/*
@@ -17,7 +17,7 @@ function InjectSandboxScript() {
   $('#SandboxViewer').prepend('<div id="SandboxBlur" style="position: fixed;z-index:2;width:100%;height:100%;background:rgba(0,0,0,0.5)"></div>');
   $('#SandboxViewer').append('<div id="SandboxContent" style="position: fixed; overflow: scroll; z-index: 3; width: 100%; height: 100%;box-sizing:border-box;top: 50%;left: 50%;-webkit-transform: translateY(-50%) translateX(-50%);transform: translateY(-50%) translateX(-50%);background: #FAFAFA;padding: 1.2em;display: -webkit-flex;display: flex;"><div style="color: gray;position: fixed;cursor: pointer;top: 0px;left: 5px;font-size: 14px;" id="closeviewer">x</div><span id="USERLOAD">Loading...</span></div>');
 
-  $(".topbar .topbar-wrapper .network-items").append('<a id="SandboxViewerToggle" class="topbar-icon yes-hover" style="z-index:1;width: 36px; background-image: url(http://i.stack.imgur.com/lBskr.png); background-size: 19px 19px; background-position: 8px 7px"></a>');
+  $(".topbar .topbar-wrapper .network-items").append('<a id="SandboxViewerToggle" class="topbar-icon yes-hover" style="z-index:1;width: 36px; background-image: url(https://i.stack.imgur.com/lBskr.png); background-size: 19px 19px; background-position: 8px 7px"></a>');
 
   var POSTCOUNTER = 0;
 
@@ -52,7 +52,7 @@ function InjectSandboxScript() {
     GM_xmlhttpRequest({
       method: "POST",
       data:"qualityBanWarningShown=False&priorAttemptCount=0&title="+title+"&post-text="+encodeURIComponent(body)+"&fkey=" + StackExchange.options.user.fkey + "&author=&wmd-input-42=&tagnames=" + body.match(/\[tag:[^\]]+/g).map(function(l){return l.slice(5);}).join(","),
-      url: "http://codegolf.stackexchange.com/questions/ask/submit/",
+      url: "https://codegolf.stackexchange.com/questions/ask/submit/",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
       },
@@ -60,7 +60,7 @@ function InjectSandboxScript() {
         var url  = (response.responseText.match(/<meta property="og:url" content="([^"]+)/)||[])[1];
         var title= $(response.responseText).find("title").text();
         var win = window.open(url, title || "Post From the Sandbox");
-        VotePost("http://meta.codegolf.stackexchange.com/posts/" + id, 10);
+        VotePost("https://meta.codegolf.stackexchange.com/posts/" + id, 10);
         win.document.write(response.responseText);
       }
     });
@@ -79,8 +79,8 @@ wmd-input-42=
 
   function ConstructPost(post) {
     $(".FVoteActive").removeClass("FVoteActive");
-    $(".FVoteUp").attr('src', 'http://i.stack.imgur.com/EQ1ko.png');
-    $(".FVoteDown").attr('src', 'http://i.stack.imgur.com/OwtQb.png');
+    $(".FVoteUp").attr('src', 'https://i.stack.imgur.com/EQ1ko.png');
+    $(".FVoteDown").attr('src', 'https://i.stack.imgur.com/OwtQb.png');
     return '<div>' + post.body + '</div>';
   }
 
@@ -91,7 +91,7 @@ wmd-input-42=
     if (Comments.length > LIMIT) a = true;
     $("#SandboxPreviewComments").html( (a ? '<div style="padding: 5px 2px; border-bottom: 1px solid #DDD; margin-bottom: 3px;"><a id="SPshow">(show all)</a></div>' : "") + Comments.map(function(comment,index,a) {
       return '<div class="'+(a.length-index>LIMIT?"SPrevH":"")+'" style="'+(a.length-index>LIMIT?"display:none;":"")+'padding: 5px 2px; border-bottom: 1px solid #DDD; margin-bottom: 3px;">'+
-        '<a href="http://codegolf.stackexchange.com/users/'+comment.userid+'">' + comment.user +
+        '<a href="https://codegolf.stackexchange.com/users/'+comment.userid+'">' + comment.user +
         '</a>: <span>'+comment.text+'</span> &mdash; <a href="'+comment.link+'">'+
         TimeSince(comment.timestamp)+'</a> </div>'
     }).join("\n") + "<div><textarea id=\"FCText\" placeholder=\"Comment...\" style=\"height: 30px;width: 80%;outline: none;border-radius: 2px;\"></textarea><button id=\"FComment\" style=\"vertical-align: top;max-width: 20%;margin-left: 8px;\">Post</button></div>");
@@ -136,8 +136,8 @@ wmd-input-42=
       '<button id="FPREV" disabled>Previous Challenge</button> <button class="sandboxbtn FNEXT">Next</button><button id="FHIDE">Don\'t show this again</button></div>'+
       '<div id="FBWrapper" style="text-align: right; float: right; margin-top: 15px;">' +
 
-      '<img style="height: 40px; cursor: pointer" src="http://i.stack.imgur.com/EQ1ko.png" class="FVoteUp">' +
-      '<img style="height: 40px; cursor: pointer" src="http://i.stack.imgur.com/OwtQb.png" class="FVoteDown">'+
+      '<img style="height: 40px; cursor: pointer" src="https://i.stack.imgur.com/EQ1ko.png" class="FVoteUp">' +
+      '<img style="height: 40px; cursor: pointer" src="https://i.stack.imgur.com/OwtQb.png" class="FVoteDown">'+
       '</div></div>'+
 
       '<div id="FBOwner" style="display: none"></div>' +
@@ -175,33 +175,33 @@ wmd-input-42=
       $(".FNEXT").click();
     });
     $(document).on('click', ".FVoteUp:not(.FVoteActive)", function() {
-      VotePost("http://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, 2);
+      VotePost("https://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, 2);
       // PopupDisplay("+1'd"); 
       $(".FVoteActive").removeClass("FVoteActive");
-      $(".FVoteDown").attr('src', 'http://i.stack.imgur.com/OwtQb.png');
-      $(".FVoteUp").attr('src', 'http://i.stack.imgur.com/iu7y5.png');
+      $(".FVoteDown").attr('src', 'https://i.stack.imgur.com/OwtQb.png');
+      $(".FVoteUp").attr('src', 'https://i.stack.imgur.com/iu7y5.png');
       $(".FVoteUp").addClass("FVoteActive");
     });
     $(document).on('click', ".FVoteDown:not(.FVoteActive)", function() {
-      VotePost("http://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, 3);
+      VotePost("https://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, 3);
       // PopupDisplay("-1'd"); 
       $(".FVoteActive").removeClass("FVoteActive");
-      $(".FVoteUp").attr('src', 'http://i.stack.imgur.com/EQ1ko.png');
-      $(".FVoteDown").attr('src', 'http://i.stack.imgur.com/LyK6V.png');
+      $(".FVoteUp").attr('src', 'https://i.stack.imgur.com/EQ1ko.png');
+      $(".FVoteDown").attr('src', 'https://i.stack.imgur.com/LyK6V.png');
       $(".FVoteDown").addClass("FVoteActive");
     });
     $(document).on('click', ".FVoteActive", function() {
-      VotePost("http://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, 0);
+      VotePost("https://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, 0);
       // PopupDisplay("&plusmn;0");
       $(".FVoteActive").removeClass("FVoteActive");
-      $(".FVoteUp").attr('src', 'http://i.stack.imgur.com/EQ1ko.png');
-      $(".FVoteDown").attr('src', 'http://i.stack.imgur.com/OwtQb.png');
+      $(".FVoteUp").attr('src', 'https://i.stack.imgur.com/EQ1ko.png');
+      $(".FVoteDown").attr('src', 'https://i.stack.imgur.com/OwtQb.png');
     })
     $("#FComment").click(function() {
-      CommentPost("http://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, $("#FCText").val());
+      CommentPost("https://meta.codegolf.stackexchange.com/posts/" + posts[POSTCOUNTER].id, $("#FCText").val());
     });
     $(".FLink").click(function(){ window.open(posts[POSTCOUNTER].url, "_blank"); });
-    $(".Fmtom").click(function(){ Request("GET", "http://api.stackexchange.com/2.2/answers/"+$(this).data('postid')+"?order=desc&sort=activity&key=Ccn4VoktkZPX*Haf3)iubw((&site=meta.codegolf&filter=!GeEyUcJFJeRCA", function(response) {
+    $(".Fmtom").click(function(){ Request("GET", "https://api.stackexchange.com/2.2/answers/"+$(this).data('postid')+"?order=desc&sort=activity&key=Ccn4VoktkZPX*Haf3)iubw((&site=meta.codegolf&filter=!GeEyUcJFJeRCA", function(response) {
       var res = JSON.parse(response.responseText).items[0].body_markdown;
       PostPost(GetPostTitle(res), res, $(this).data('postid'));
     }); });
@@ -211,7 +211,7 @@ wmd-input-42=
     $('#SandboxViewer').fadeIn(100);
     if (OPENED === false) {
       OPENED = true;
-      $("#SandboxViewerToggle").css("background-image", "url(http://i.stack.imgur.com/lBskr.png)");
+      $("#SandboxViewerToggle").css("background-image", "url(https://i.stack.imgur.com/lBskr.png)");
       GetChallenges(StackExchange.options.user.userId, function(posts, anposts) {
         var HTML = "";
         HTML += '<h1>Your Sandboxed Posts</h1><div><ul>' + (posts.map(function(post, index) {
@@ -265,7 +265,7 @@ wmd-input-42=
 
         HTML += '<h1>Latest Activity</h1><div>' + (GetComments(posts).map(function(comment, i, a) {
           return '<div '+
-            (i == a.length - 1 ? "" : ' style="border-bottom: 1px solid #DDD; padding-bottom: 8px; margin-bottom: 8px"')+'><b><a href="' + comment.postlink + '">' + comment.post + '</a></b>, <a href="http://codegolf.stackexchange.com/users/' + comment.userid + '">' + comment.user + '</a>: ' + comment.text + " - <a href=\"" + comment.link + "\">" + TimeSince(comment.timestamp) + "</a></div>";
+            (i == a.length - 1 ? "" : ' style="border-bottom: 1px solid #DDD; padding-bottom: 8px; margin-bottom: 8px"')+'><b><a href="' + comment.postlink + '">' + comment.post + '</a></b>, <a href="https://codegolf.stackexchange.com/users/' + comment.userid + '">' + comment.user + '</a>: ' + comment.text + " - <a href=\"" + comment.link + "\">" + TimeSince(comment.timestamp) + "</a></div>";
         }).join("") || "<div>You currently have no Sandboxed posts</div>") + '</div>';
         $("#SandboxContent").prepend('<div style="width: 33%; -webkit-flex-direction: column; flex-direction: column; overflow: auto;">' + HTML + "</div>");
       });
@@ -356,7 +356,7 @@ wmd-input-42=
         var wsd = JSON.parse(JSON.parse(e.data).data);
         if (wsd.a === "answer-add") {
           // answer added
-          $("#SandboxViewerToggle").css("background-image", "url(http://i.stack.imgur.com/COtrF.png)");
+          $("#SandboxViewerToggle").css("background-image", "url(https://i.stack.imgur.com/COtrF.png)");
         }
       } catch(e) {
         console.log("error during websocket update");
@@ -380,7 +380,7 @@ wmd-input-42=
         }));
       });
     } else {
-      Request("GET", "http://api.stackexchange.com/2.2/search/excerpts?order=desc&sort=activity&title=Sandbox%20for%20Proposed%20Challenges&user="+StackExchange.options.user.userId+"&site=meta.codegolf", function(data) {
+      Request("GET", "https://api.stackexchange.com/2.2/search/excerpts?order=desc&sort=activity&title=Sandbox%20for%20Proposed%20Challenges&user="+StackExchange.options.user.userId+"&site=meta.codegolf", function(data) {
         var items = JSON.parse(data.response).items;
         function Loop(i, a) {
           if (i > items.length - 1) {
@@ -388,7 +388,7 @@ wmd-input-42=
               callback(a, nonposts);
             });
           } else {
-            Request("GET", 'http://api.stackexchange.com/2.2/answers/'+items[i].answer_id+'?pagesize=100&order=desc&sort=activity&site=meta.codegolf&filter=!-2qNq(tTGQYRU3SZ87hedUU)5htvSK6RNae3(IkBC-M8i', function(r) {
+            Request("GET", 'https://api.stackexchange.com/2.2/answers/'+items[i].answer_id+'?pagesize=100&order=desc&sort=activity&site=meta.codegolf&filter=!-2qNq(tTGQYRU3SZ87hedUU)5htvSK6RNae3(IkBC-M8i', function(r) {
               a.push(JSON.parse(r.response).items[0]);
               Loop(i + 1, a);
             });
